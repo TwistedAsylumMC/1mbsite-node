232c25a3c8577c7079fef2657aca8d25c3bf11d1
Nathaniel Fredericks <40307305+nathfreder@users.noreply.github.com>
2019-03-20 19:05:45 -0300

    Revert "Format API.js"
    This reverts commit 4a0d4ac4ae8bc931bec689fd68a96146f56efdaf.

--- API.js
+++ API.js
@@ -1,93 +1,94 @@
-const querystring = require('querystring');
-const request = require('request');
+const querystring = require("querystring");
+const request = require("request");
 
 class API {
-  constructor(username, key) {
-    this.api_url = 'https://api.1mb.site';
 
-    this.username = username;
-    this.key = key;
+	constructor(username, key){
+		this.api_url = "https://api.1mb.site";
 
-    this.verifyKey((result) => {
-      if (result.error) {
-        console.log('Unable to verify key: ' + result.error);
-      }
-    });
-  }
+		this.username = username;
+		this.key = key;
 
-  verifyKey(callback) {
-    let postData = {
-      action: 'keyverify',
-      username: this.username,
-      key: this.key,
-    };
-    let options = {
-      url: this.api_url,
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Content-Length': Buffer.byteLength(querystring.stringify(postData)),
-      },
-      form: postData,
-    };
-    request(options, (error, _response, body) => {
-      if (error) {
-        throw new Error(error);
-      } else {
-        callback(JSON.parse(body));
-      }
-    });
-  }
+		this.verifyKey(function(result){
+			if(result.error){
+				console.log("Unable to verify key: " + result.error);
+			}
+		});
+	}
 
-  deploy(resource, code, callback) {
-    let postData = {
-      action: 'deploy',
-      site: this.username,
-      key: this.key,
-      resource: resource,
-      code: code,
-    };
-    let options = {
-      url: this.api_url,
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Content-Length': Buffer.byteLength(querystring.stringify(postData)),
-      },
-      form: postData,
-    };
-    request(options, (error, _response, body) => {
-      if (error) {
-        throw new Error(error);
-      } else {
-        callback(JSON.parse(body));
-      }
-    });
-  }
+	verifyKey(callback){
+		let postData = {
+			action: "keyverify",
+			username: this.username,
+			key: this.key
+		};
+		let options = {
+			url: this.api_url,
+			method: "POST",
+			headers: {
+				"Content-Type": "application/x-www-form-urlencoded",
+				"Content-Length": Buffer.byteLength(querystring.stringify(postData))
+			},
+			form: postData
+		};
+		request(options, function(error, response, body){
+			if(error){
+				throw new Error(error);
+			}else{
+				callback(JSON.parse(body));
+			}
+		});
+	}
 
-  viewSite(site, resource, callback) {
-    let postData = {
-      action: 'view',
-      site: site,
-      resource: resource,
-    };
-    let options = {
-      url: this.api_url,
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Content-Length': Buffer.byteLength(querystring.stringify(postData)),
-      },
-      form: postData,
-    };
-    request(options, (error, _response, body) => {
-      if (error) {
-        throw new Error(error);
-      } else {
-        callback(JSON.parse(body));
-      }
-    });
-  }
+	deploy(resource, code, callback){
+		let postData = {
+			action: "deploy",
+			site: this.username,
+			key: this.key,
+			resource: resource,
+			code: code
+		};
+		let options = {
+			url: this.api_url,
+			method: "POST",
+			headers: {
+				"Content-Type": "application/x-www-form-urlencoded",
+				"Content-Length": Buffer.byteLength(querystring.stringify(postData))
+			},
+			form: postData
+		};
+		request(options, function(error, response, body){
+			if(error){
+				throw new Error(error);
+			}else{
+				callback(JSON.parse(body));
+			}
+		});
+	}
+
+	viewSite(site, resource, callback){
+		let postData = {
+			action: "view",
+			site: site,
+			resource: resource
+		};
+		let options = {
+			url: this.api_url,
+			method: "POST",
+			headers: {
+				"Content-Type": "application/x-www-form-urlencoded",
+				"Content-Length": Buffer.byteLength(querystring.stringify(postData))
+			},
+			form: postData
+		};
+		request(options, function(error, response, body){
+			if(error){
+				throw new Error(error);
+			}else{
+				callback(JSON.parse(body));
+			}
+		});
+	}
 }
 
 module.exports = API;
